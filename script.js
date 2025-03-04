39db115c7430ba7e8bfbe26b61a66d2440b091f9
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-04 18:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
     output.innerHTML = `<tr id="loading"><td colspan="2">Loading...</td></tr>`;
 
-    function createPromise(index) {
-        let time = (Math.random() * 2 + 1).toFixed(3);
-        return new Promise((resolve) => {
-            setTimeout(() => resolve({ index, time }), time * 1000);
-        });
-    }
+    // function createPromise(index) {
+    //     let time = (Math.random() * 2 + 1).toFixed(3);
+    //     return new Promise((resolve) => {
+    //         setTimeout(() => resolve({ index, time }), time * 1000);
+    //     });
+    // }
+	function createPromise(index) {
+    const time = (Math.random() * 1 + 1).toFixed(3); // adjust the maximum time to 2 seconds
+    return new Promise(resolve => {
+        setTimeout(() => resolve({ index, time }), time * 1000);
+    });
+}
 
     let promises = [createPromise(1), createPromise(2), createPromise(3)];
 
-    Promise.all(promises).then((results) => {
-        document.getElementById("loading").remove();
+    Promise.all(promises).then(results => {
+    document.getElementById("loading").remove();
 
-        let maxTime = 0;
+    let maxTime = 0;
 
-        results.forEach((result) => {
-            maxTime = Math.max(maxTime, parseFloat(result.time));
-            output.innerHTML += `<tr>
-									<td>Promise ${result.index}</td>
-									<td>${result.time}</td>
-								</tr>`;
-        });
+    // Sort the results based on the promise index
+    results.sort((a, b) => a.index - b.index);
 
+    results.forEach((result) => {
+        maxTime = Math.max(maxTime, parseFloat(result.time));
         output.innerHTML += `<tr>
-								<td>Total</td>
-								<td>${maxTime.toFixed(3)}</td>
-							</tr>`;
+                                <td>Promise ${result.index}</td>
+                                <td>${result.time}</td>
+                            </tr>`;
     });
+
+    output.innerHTML += `<tr>
+                            <td>Total</td>
+                            <td>${maxTime.toFixed(3)}</td>
+                         </tr>`;
+});
+	
 });
 
 
