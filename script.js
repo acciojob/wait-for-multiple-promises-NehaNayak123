9ffa7c9eb82dfb7593df0a95df7c9aa3378ea08b
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-04 17:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,21 +21,37 @@ const promises = [createPromise(1), createPromise(2), createPromise(3)];
 
 // Waiting for all promises to resolve
 const startTime = performance.now();
+
+// Promise.all(promises).then(results => {
+//     const totalTime = ((performance.now() - startTime) / 1000).toFixed(3);
+//     output.innerHTML = "";
+
+//     // Append resolved results to the table
+//     results.forEach(({ index, time }) => {
+//         const row = document.createElement("tr");
+//         row.innerHTML = `<td>Promise ${index}</td><td>${time}</td>`;
+        
+// 		output.appendChild(row);
+//     });
+
+//     // Append total time row
+//     const totalRow = document.createElement("tr");
+//     totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
+// 	output.appendChild(totalRow);
+// });
+
 Promise.all(promises).then(results => {
     const totalTime = ((performance.now() - startTime) / 1000).toFixed(3);
-    output.innerHTML = "";
+    let resultsHTML = "";
 
-    // Append resolved results to the table
+    // Append resolved results to the string
     results.forEach(({ index, time }) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `<td>Promise ${index}</td><td>${time}</td>`;
-        
-		output.appendChild(row);
+        resultsHTML += `<tr><td>Promise ${index}</td><td>${time}</td></tr>`;
     });
 
-    // Append total time row
-    const totalRow = document.createElement("tr");
-    totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
-	output.appendChild(totalRow);
-});
+    // Append total time row to the string
+    resultsHTML += `<tr><td>Total</td><td>${totalTime}</td></tr>`;
 
+    // Set the table body's innerHTML to the results string
+    output.innerHTML = resultsHTML;
+});
